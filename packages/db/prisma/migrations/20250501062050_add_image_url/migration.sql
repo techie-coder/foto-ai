18ac68552ce82cd768cedee8b937784d4eed798c
Saswata Biswas <saswata.biswas20@gmail.com>
2025-05-03 12:51:10 +0530

    del migrations

--- packages/db/prisma/migrations/20250501062050_add_image_url/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "OutputImages" ALTER COLUMN "imageUrl" SET DEFAULT '';
--- packages/db/prisma/migrations/20250501103455_add_fal_ai_fields/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "Model" ADD COLUMN     "falAiReuestId" TEXT,
-ADD COLUMN     "tensorPath" TEXT,
-ADD COLUMN     "trainingStatus" "StatusTypes" NOT NULL DEFAULT 'Pending',
-ADD COLUMN     "triggerWord" TEXT;
-
--- AlterTable
-ALTER TABLE "OutputImages" ADD COLUMN     "falAiReuestId" TEXT;
